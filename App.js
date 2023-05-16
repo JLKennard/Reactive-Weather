6c88e92930ec8a0609b7d08e4bd17aa4b908635d
JLKennard <jessicakennard@hotmail.co.uk>
2023-05-17 00:12:40 +0100

    map cities

--- App.js
+++ App.js
@@ -7,7 +7,11 @@ function App() {
     <>
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
-      <div className="app">{/* Render components here */}</div>
+      <div className="app">
+        {cities.map((cities) => (
+          <WeatherCard data={cities} />
+        ))}
+      </div>
     </>
   );
 }
