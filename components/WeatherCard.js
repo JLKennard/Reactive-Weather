9047955986fcbca53f217bf1efebd82071e5e439
JLKennard <jessicakennard@hotmail.co.uk>
2023-05-17 00:50:27 +0100

    import assets

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,5 +1,8 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg";
+import cloudy from '../assets/Cloudy.svg'
+import partly_cloudy from '../assets/PartlyCloudy.svg'
+import rainy from '../assets/Rainy.svg'
 
 function WeatherCard({data: { city, temperature, forecast }}) {
   return (
