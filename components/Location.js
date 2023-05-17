b521ca90a18298bba1f064b3a2d13ffb52276c2a
JLKennard <jessicakennard@hotmail.co.uk>
2023-05-17 16:04:07 +0100

    error handelling if no found location

--- components/Location.js
+++ components/Location.js
@@ -1,37 +1,46 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg";
-import rainy from '../assets/Rainy.svg'
-import partly_cloudy from '../assets/PartlyCloudy.svg'
-import cloudy from '../assets/Cloudy.svg'
+import rainy from "../assets/Rainy.svg";
+import partly_cloudy from "../assets/PartlyCloudy.svg";
+import cloudy from "../assets/Cloudy.svg";
 
-function Location({data, location, setLocation}){
+function Location({ data, location, setLocation }) {
+  const foundCity = data.find((city) => city.city === location) ?? false;
 
-    const foundCity = data.find((element) => element.city === location)
+  const weather =
+    foundCity.forecast === "Sunny"
+      ? sunny
+      : foundCity.forecast === "Rainy"
+      ? rainy
+      : foundCity.forecast === "Partly cloudy"
+      ? partly_cloudy
+      : foundCity.forecast === "Cloudy"
+      ? cloudy
+      : null;
 
-    const weather = 
-    foundCity.forecast === 'Sunny' ? sunny : 
-    foundCity.forecast === 'Rainy' ? rainy : 
-    foundCity.forecast === 'Partly cloudy' ? partly_cloudy : 
-    foundCity.forecast === 'Cloudy' ? cloudy: 
-    null
-
-    return (
-        <div className="card">
-          <div className="img-container">
-            <img
-              className="card-img-top"
-              src={weather}
-              alt="Card image cap"
-              id="icon"
-            />
-          </div>
-          <div class="card-body">
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img
+          className="card-img-top"
+          src={weather}
+          alt="Card image cap"
+          id="icon"
+        />
+      </div>
+      <div class="card-body">
+        {foundCity ? (
+          <>
             <h3 className="card-title">{foundCity.city}</h3>
             <h5 className="card-text">{foundCity.temperature}</h5>
             <h5 className="card-text">{foundCity.forecast}</h5>
-          </div>
-        </div>
-      );
+          </>
+        ) : (
+          "Location not found"
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Location
+export default Location;
