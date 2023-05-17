ed4cb3ae69c528b586a2fdcb23e54dc953fb396d
JLKennard <jessicakennard@hotmail.co.uk>
2023-05-17 16:12:57 +0100

    updated title in app return

--- App.js
+++ App.js
@@ -6,23 +6,16 @@ import Location from "./components/Location";
 import Form from "./components/Form";
 
 function App() {
-
-  const [location, setLocation] = useState('New York City')
-
-
+  const [location, setLocation] = useState("New York City");
 
   return (
     <>
-      <h1 className="title">REACTIVE WEATHER</h1>
+      <h1 className="title">JESSICA'S REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
       <div className="app">
-        
-        <Form location={location}
-        setLocation={setLocation}/>
+        <Form location={location} setLocation={setLocation} />
 
-        <Location data={cities} 
-        location={location}
-        setLocation={setLocation}/>
+        <Location data={cities} location={location} setLocation={setLocation} />
         {/* {cities.map((cities) => (
           <WeatherCard data={cities} />
         ))} */}
