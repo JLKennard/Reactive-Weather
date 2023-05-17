080c9b70ac894e243ad10c9ea2bb5119612bc56d
JLKennard <jessicakennard@hotmail.co.uk>
2023-05-17 08:31:27 +0100

    sgv icon conditional

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,16 +1,26 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg";
-import cloudy from '../assets/Cloudy.svg'
-import partly_cloudy from '../assets/PartlyCloudy.svg'
 import rainy from '../assets/Rainy.svg'
+import partly_cloudy from '../assets/PartlyCloudy.svg'
+import cloudy from '../assets/Cloudy.svg'
+
+
 
 function WeatherCard({data: { city, temperature, forecast }}) {
+  
+  const weather = 
+  forecast === 'Sunny' ? sunny : 
+  forecast === 'Rainy' ? rainy : 
+  forecast === 'Partly cloudy' ? partly_cloudy : 
+  forecast === 'Cloudy' ? cloudy: 
+  null
+ 
   return (
     <div className="card">
       <div className="img-container">
         <img
           className="card-img-top"
-          src={sunny}
+          src={weather}
           alt="Card image cap"
           id="icon"
         />
