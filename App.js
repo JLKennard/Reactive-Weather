5501ffe8573b92d8dadbfea21104d6e304b641c1
JLKennard <jessicakennard@hotmail.co.uk>
2023-05-17 15:17:24 +0100

    import form and render form in app.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import cities from "./data";
 import WeatherCard from "./components/WeatherCard";
 import Location from "./components/Location";
+import Form from "./components/Form";
 
 function App() {
 
@@ -15,7 +16,7 @@ function App() {
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
       <div className="app">
-        
+        <Form />
         <Location data={cities} 
         location={location}
         setLocation={setLocation}/>
